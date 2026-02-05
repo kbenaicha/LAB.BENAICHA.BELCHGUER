6117cdc66c88c97b71ff3bc792e05015702ee13d
kbenaicha <154835526+kbenaicha@users.noreply.github.com>
2026-02-05 10:39:10 +0100

    lab1

--- lab/src/controllers/user.js
+++ lab/src/controllers/user.js
@@ -5,11 +5,13 @@ module.exports = {
     // Check parameters
     if(!user.username)
       return callback(new Error("Wrong user parameters"), null)
+
     // Create User schema
     const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
     }
+
     // Save to DB
     // TODO check if user already exists
     db.hmset(user.username, userObj, (err, res) => {
@@ -17,7 +19,30 @@ module.exports = {
       callback(null, res) // Return callback
     })
   },
-  // get: (username, callback) => {
-  //   // TODO create this method
-  // }
+
+  get: (username, callback) => {
+    // Check parameters
+    if (!username) {
+      return callback(new Error("Wrong user parameters"), null)
+    }
+
+    // Read user from DB (Redis hash)
+    db.hgetall(username, (err, res) => {
+      if (err) return callback(err, null)
+
+      // If user doesn't exist, Redis returns null
+      if (!res) {
+        return callback(new Error("User not found"), null)
+      }
+
+      // Rebuild full user object (include username)
+      const userObj = {
+        username: username,
+        firstname: res.firstname,
+        lastname: res.lastname
+      }
+
+      callback(null, userObj)
+    })
+  }
 }
--- lab/test/user.controller.js
+++ lab/test/user.controller.js
@@ -44,18 +44,41 @@ describe('User', () => {
   })
 
   // TODO Create test for the get method
-  // describe('Get', ()=> {
-  //   
-  //   it('get a user by username', (done) => {
-  //     // 1. First, create a user to make this unit test independent from the others
-  //     // 2. Then, check if the result of the get method is correct
-  //     done()
-  //   })
-  //
-  //   it('cannot get a user when it does not exist', (done) => {
-  //     // Chech with any invalid user
-  //     done()
-  //   })
-  //
-  // })
+  describe('Get', () => {
+
+    it('get a user by username', (done) => {
+      // 1) First, create a user (this test must be independent)
+      const user = {
+        username: 'sergkudinov',
+        firstname: 'Sergei',
+        lastname: 'Kudinov'
+      }
+
+      userController.create(user, (err, result) => {
+        expect(err).to.be.equal(null)
+        expect(result).to.be.equal('OK')
+
+        // 2) Then, try to get the same user
+        userController.get('sergkudinov', (err2, result2) => {
+          expect(err2).to.be.equal(null)
+          expect(result2).to.not.be.equal(null)
+
+          // If Redis stores JSON, result2 is a string => parse it
+          const parsed = (typeof result2 === 'string') ? JSON.parse(result2) : result2
+          expect(parsed).to.deep.equal(user)
+
+          done()
+        })
+      })
+    })
+
+    it('cannot get a user when it does not exist', (done) => {
+      userController.get('unknown_user', (err, result) => {
+        expect(err).to.not.be.equal(null)
+        expect(result).to.be.equal(null)
+        done()
+      })
+    })
+
+  })
 })
